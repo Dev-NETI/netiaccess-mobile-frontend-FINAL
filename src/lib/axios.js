dad57d67ab0110c2ffc969d47d130aaf147bc8e2
MysticMaccc <Cosmicsher96@gmail.com>
2024-07-23 10:52:13 +0800

    Finalize configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,13 +1,13 @@
-import Axios from 'axios'
+import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        Accept: 'application/json',
-    },
-    withCredentials: true,
-    withXSRFToken: true
-})
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
+  },
+  withCredentials: true,
+  withXSRFToken: true,
+});
 
-export default axios
+export default axios;
